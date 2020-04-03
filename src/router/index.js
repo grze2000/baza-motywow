66ed2a74c1124976548dc5af79ef4a86c6a33bf1
Programista3 <programista333@gmail.com>
2020-04-03 13:48:46 +0200

    Add login form

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -10,14 +11,15 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  /*{
+  {
     path: '/login',
     name: 'Login',
+    component: Login
   },
   {
     path: '/admin',
     name: 'Admin'
-  }*/
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+        <v-container fluid class="fill-height grey lighten-5">
+            <v-row align="center" justify="center">
+                <v-col cols="12" md="4">
+                    <v-card class="" raised>
+                        <v-toolbar flat dark color="orange darken-2">
+                            <v-toolbar-title>Logowanie</v-toolbar-title>
+                        </v-toolbar>
+                        <v-card-text>
+                            <v-form>
+                                <v-text-field label="Nazwa użytkownika" prepend-icon="person"/>
+                                <v-text-field type="password" label="Hasło" prepend-icon="lock"/>
+                            </v-form>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-spacer/>
+                            <v-btn color="orange darken-2" dark>Zaloguj się</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
